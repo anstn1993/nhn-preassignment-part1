7b5e256f647490b7a92b9e8713fcde8eefecd1ed
NHN <nhn@NHNui-MacBookPro-2.local>
2021-01-19 17:00:02 +0900

    feat : completed chapter 3 of part1 #2

--- src/main/java/org/zerock/ex2/controller/SampleController.java
+++ src/main/java/org/zerock/ex2/controller/SampleController.java
@@ -1,12 +1,78 @@
 package org.zerock.ex2.controller;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.ex2.dto.SampleDTO;
 
-@RestController
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Controller
+@RequestMapping("/sample")
+@Log4j2
 public class SampleController {
-    @GetMapping("/hello")
-    public String[] hello() {
-        return new String[]{"Hello", "World"};
+    @GetMapping({"/ex1", "/ex2"})
+    public void ex1(Model model) {
+        List<SampleDTO> list = IntStream.rangeClosed(1, 20).asLongStream().mapToObj(i -> {
+            return SampleDTO.builder()
+                    .sno(i)
+                    .first("First.." + i)
+                    .last("Last.." + i)
+                    .regTime(LocalDateTime.now())
+                    .build();
+        }).collect(Collectors.toList());
+
+        log.info("ex1...............");
+
+        model.addAttribute("list", list);
+    }
+
+    @GetMapping({"/ex2", "/exLink", "/exFormat"})
+    public void exModel(Model model) {
+        List<SampleDTO> list = IntStream.rangeClosed(1, 20).asLongStream().mapToObj(i -> {
+            return SampleDTO.builder()
+                    .sno(i)
+                    .first("First.." + i)
+                    .last("Last.." + i)
+                    .regTime(LocalDateTime.now())
+                    .build();
+        }).collect(Collectors.toList());
+
+        log.info("ex1...............");
+
+        model.addAttribute("list", list);
+    }
+
+    @GetMapping("/exInline")
+    public String exInline(RedirectAttributes redirectAttributes) {
+        log.info("exInline..............");
+
+        SampleDTO dto = SampleDTO.builder()
+                .sno(100L)
+                .first("First..100")
+                .last("Last..100")
+                .regTime(LocalDateTime.now())
+                .build();
+
+        redirectAttributes.addFlashAttribute("result", "success");
+        redirectAttributes.addFlashAttribute("dto", dto);
+
+        return "redirect:/sample/ex3";
+    }
+
+    @GetMapping("/ex3")
+    public void ex3() {
+        log.info("ex3");
+    }
+
+    @GetMapping({"/exLayout1", "/exLayout2", "/exLayout3", "exTemplate", "/exSidebar"})
+    public void exLayout1() {
+        log.info("exLayout...........");
     }
 }
